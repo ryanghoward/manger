e4aca6c27b1989768d0b47112dc091a9c9201fd6
Ryan Howard <ryanghoward@protonmail.com>
2023-02-17 16:22:58 -0800

    done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
         {/* Overlay */}
         <div className='absolute w-full h-full text-gray-200 mx-h-[500px] bg-black/40 flex flex-col justify-center'>
           <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
-            Your <span className='text-pink-500'>Favorite</span>
+            All Your <span className='text-pink-500'>Favorite</span>
           </h1>
           <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
             {/* <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-900 to-cyan-500'> */}
